0f5e289f9d367ec385d1ec998aeea87c81b095cb
BlackBeardEC <rich.stadnick@gmail.com>
2017-06-20 15:25:25 -0400

    Style and update copy on About page.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,14 @@ var React = require('react');
 var About = (props)=>{
   return (
     <div>
-      <h3>About</h3>
+      <h1 className='text-center'>About</h1>
       <p>Welcome to the About page.</p>
+      <p>This is a simple Weather application written in React. I have built this as a Udemy course project.</p>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li><a href="https://facebook.github.io/react" target="_blank">React</a> - This is the JavaScript framework used.</li>
+        <li><a href="https://openweathermap.org" target="_blank">Open Weather Map</a> - This is the API used to fetch weather by City Name.</li>
+      </ul>
     </div>
   );
 };
